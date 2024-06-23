272a7feaaedf4e3316f8f79360df691962f70e1c
murilo-rossi <murilorossi31@gmail.com>
2024-06-23 16:13:19 -0300

    Update Caminhos.java

--- JavaCafe/src/Paths/Caminhos.java
+++ JavaCafe/src/Paths/Caminhos.java
@@ -1,9 +1,10 @@
 package Paths;
 
+import java.nio.file.Paths;
+
 //Caminhos para os arquivos de pedidos e inventários do café
 public class Caminhos {
     private static final String _PATH = System.getProperty("user.dir");
-    public static final String INVENTARIO_FILE = _PATH + "\\src\\Dados\\inventario.txt";
-    public static final String PEDIDOS_FILE = _PATH + "\\src\\Dados\\pedidos.txt";
+    public static final String INVENTARIO_FILE = Paths.get(_PATH, "src", "Dados", "inventario.txt").toString();
+    public static final String PEDIDOS_FILE = Paths.get(_PATH, "src", "Dados", "pedidos.txt").toString();
 }
-
