d989a0ee81f86424bdf9b8bb4a9a57c2526a0476
murilo-rossi <murilorossi31@gmail.com>
2024-06-23 18:58:43 -0300

    Update ClienteMain.java

--- JavaCafe/src/Main/ClienteMain.java
+++ JavaCafe/src/Main/ClienteMain.java
@@ -1,14 +1,13 @@
+package Main;
+
 import Telas.Listagem;
 import Paths.Caminhos;
 import Telas.TelaCliente;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.IOException;
 
-public class ClienteMain extends JFrame {
+public class ClienteMain{
     private Listagem listagem;
 
     public ClienteMain() {
@@ -26,44 +25,7 @@ public class ClienteMain extends JFrame {
             e.printStackTrace();
         }
 
-        // Configurações da janela principal
-        setTitle("Java Café - Cliente");
-        setSize(300, 80);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
-
-        // Criação do painel de botões
-        JPanel panel = new JPanel();
-        JButton clienteButton = new JButton("Abrir Tela Cliente");
-        JButton sairButton = new JButton("Encerrar");
-
-        // Ações dos botões
-        clienteButton.addActionListener(e -> abrirTelaCliente());
-        sairButton.addActionListener(e -> encerrarAplicacao());
-
-        panel.add(clienteButton);
-        panel.add(sairButton);
-
-        add(panel, BorderLayout.CENTER);
-
-        // Centraliza a janela na tela
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        setLocation((screenSize.width - getWidth()) / 2, (screenSize.height - getHeight()) / 8);
-
-        // Salva o inventário ao fechar a janela
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                try {
-                    listagem.salvarInv(Caminhos.INVENTARIO_FILE);
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-                super.windowClosing(e);
-            }
-        });
-
-        setVisible(true);
+        abrirTelaCliente();
     }
 
     private void abrirTelaCliente() {
@@ -72,9 +34,5 @@ public class ClienteMain extends JFrame {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(ClienteMain::new);
-    }
-
-    private void encerrarAplicacao() {
-        System.exit(0);
     }
 }
