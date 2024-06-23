796e72a8b7f6c60c0eaeed5584dc8afd61757096
murilo-rossi <murilorossi31@gmail.com>
2024-06-23 18:58:53 -0300

    Update GerenteMain.java

--- JavaCafe/src/Main/GerenteMain.java
+++ JavaCafe/src/Main/GerenteMain.java
@@ -1,14 +1,14 @@
+package Main;
+
 import Telas.Listagem;
 import Paths.Caminhos;
 import Telas.TelaGerente;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.IOException;
 
-public class GerenteMain extends JFrame {
+public class GerenteMain extends Component {
     private Listagem listagem;
 
     public GerenteMain() {
@@ -26,47 +26,10 @@ public class GerenteMain extends JFrame {
             e.printStackTrace();
         }
 
-        // Configurações da janela principal
-        setTitle("Java Café - Gerente");
-        setSize(300, 80);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
-
-        // Criação do painel de botões
-        JPanel panel = new JPanel();
-        JButton gerenteButton = new JButton("Abrir Tela Gerente");
-        JButton sairButton = new JButton("Encerrar");
-
-        // Ações dos botões
-        gerenteButton.addActionListener(e -> autenticarGerente());
-        sairButton.addActionListener(e -> encerrarAplicacao());
-
-        panel.add(gerenteButton);
-        panel.add(sairButton);
-
-        add(panel, BorderLayout.CENTER);
-
-        // Centraliza a janela na tela
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        setLocation((screenSize.width - getWidth()) / 2, (screenSize.height - getHeight()) / 8);
-
-        // Salva o inventário ao fechar a janela
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                try {
-                    listagem.salvarInv(Caminhos.INVENTARIO_FILE);
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-                super.windowClosing(e);
-            }
-        });
-
-        setVisible(true);
+        autenticarDono();
     }
 
-    private void autenticarGerente() {
+    private void autenticarDono() {
         JPasswordField passwordField = new JPasswordField(10);
         int action = JOptionPane.showConfirmDialog(this, passwordField, "Digite a senha:", JOptionPane.OK_CANCEL_OPTION);
         if (action == JOptionPane.OK_OPTION) {
@@ -85,9 +48,5 @@ public class GerenteMain extends JFrame {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(GerenteMain::new);
-    }
-
-    private void encerrarAplicacao() {
-        System.exit(0);
     }
 }
