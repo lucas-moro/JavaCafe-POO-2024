a6199af35ba1095e348d28496ace308273a9f981
murilo-rossi <murilorossi31@gmail.com>
2024-06-23 11:11:25 -0300

    Update Caminhos.java
    Pega o path relativo de cada usuário

--- JavaCafe/src/Paths/Caminhos.java
+++ JavaCafe/src/Paths/Caminhos.java
@@ -1,8 +1,9 @@
 package Paths;
 
+//Caminhos para os arquivos de pedidos e inventários do café
 public class Caminhos {
-    private static final String _PATH = "C:\\Users\\Lucas\\IdeaProjects\\JavaCafe\\";
-    public static final String INVENTARIO_FILE = _PATH + "src\\Dados\\inventario.txt";
-    public static final String PEDIDOS_FILE = _PATH + "src\\Dados\\pedidos.txt";
+    private static final String _PATH = System.getProperty("user.dir");
+    public static final String INVENTARIO_FILE = _PATH + "\\src\\Dados\\inventario.txt";
+    public static final String PEDIDOS_FILE = _PATH + "\\src\\Dados\\pedidos.txt";
 }
 
